6d05ab1c83fc8c3525b22c5fd0919586d6c3c014
jerichosamuels <126803279+jerichosamuels@users.noreply.github.com>
2023-09-20 14:40:06 +0100

    Waypoint on the way to completing Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -13,8 +13,9 @@ const BlogPost = ({data, children}) => {
       <GatsbyImage 
       image={image}
       alt={data.mdx.frontmatter.hero_image_alt}/>
-      <p> Photo Credit:{""}
+      <p> Photo Credit:{" "}
       <a href={data.mdx.frontmatter.hero_image_credit_link}>
+        {data.mdx.frontmatter.hero_image_credit_text}
       </a>
       </p>
     {children}
@@ -23,22 +24,23 @@ const BlogPost = ({data, children}) => {
 }
 
 export const query = graphql`
-  query($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        hero_image_alt
-        hero_image_credit_link
-        hero_image_credit_text
-        hero_image {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
+query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }  
       }
     }
   }
+}
+
 `
 
 export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
