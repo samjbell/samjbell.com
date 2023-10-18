80e13f52c15fb36ba202d4e8f1a2d0a52fb2ca33
sambell <126803279+samjbell@users.noreply.github.com>
2023-10-18 16:12:38 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,29 +8,32 @@ import {
   navLinkText,
   siteTitle,
 } from './layout.module.css'
+
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        title
+    query {
+      site {
+        siteMetadata {
+          title
+        }
       }
     }
-  }
-`)
-  
-  
-  
-  
+  `)
   
   
   return (
     <div className={container} >
-    <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
-        <ul className={navLinkItem}>
-          <li className={navLinkItem}><Link to="/">Home</Link></li>
-          <li className={navLinkItem}><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}> Home 
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>About
+            </Link>
+          </li>
           <li className={navLinkItem}>
             <Link to="/blog" className={navLinkText}>
               Blog
