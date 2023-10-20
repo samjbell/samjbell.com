17f266296e8c984baf2ebce7baa1bf7230e20dbf
sambell <126803279+samjbell@users.noreply.github.com>
2023-10-20 15:33:44 +0100

    Update index.js
    Removed additional code for deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
@@ -20,19 +20,3 @@ export const Head = () => <Seo title="Home" />
 
 export default IndexPage
   
-=======
-import * as React from "react"
-import Layout from '../components/layout'
-
-const HomePage = () => {
-  return (
-<Layout pageTitle="Home Page"> 
-  <p> Hello and welcome to my site! </p>
-  </Layout>
-  )
-}
-export const Head = () => <title>Home Page</title>
-
-export default HomePage
->>>>>>> refs/remotes/origin/master
-
